0c070aec21e6d2f4dfd0dc0e5c162558065721f7
Vlad <tomhanter785@gmail.com>
2023-02-19 15:07:41 +0300

    /

--- Assets/Scripts/HangmanGame.cs
+++ Assets/Scripts/HangmanGame.cs
@@ -7,10 +7,17 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-namespace Hangman.Scripts
+public class HangmanGame : MonoBehaviour
 {
-      public class HangmanGame : MonoBehaviour
+    [SerializeField] TextMeshPro 
+    [SerializeField] private int hp = 7;
+    
+    private List<char> guessedLetters = new List<char>();
+    private List<char> wrongTriedLetter = new List<char>();
+
+    private string[] words =
     {
+<<<<<<< HEAD
         [SerializeField] private TextMeshProUGUI _textField;
         [SerializeField] private TextMeshProUGUI _textHp;
         [SerializeField] private TextMeshProUGUI _textLetters;
@@ -22,20 +29,40 @@ namespace Hangman.Scripts
         };
         
         [SerializeField] private int hp = 7;
+=======
+        "Cat",
+        "Dog"
+    };
+>>>>>>> parent of fe29ece (Fix)
 
-        private List<char> guessedLetters = new List<char>();
-        private List<char> wrongTriedLetter = new List<char>();
+    private string wordToGuess = "";
 
-        private string[] words =
+    private KeyCode lastKeyPressed;
+
+    private void Start()
+    {
+        var randomIndex = Random.Range(0, words.Length);
+
+        wordToGuess = words[randomIndex];
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
         {
+<<<<<<< HEAD
             "Cat",
             "Dog",
             "Orange",
             "Banana"
         };
+=======
+            // Debug.Log("Detected key code: " + e.keyCode);
+>>>>>>> parent of fe29ece (Fix)
 
-        private string wordToGuess = "";
 
+<<<<<<< HEAD
         private KeyCode lastKeyPressed;
 
         private void Start()
@@ -65,6 +92,13 @@ namespace Hangman.Scripts
 
                     lastKeyPressed = e.keyCode; // буферим нажатую клавишу в прошлый раз
                 }
+=======
+            if (e.keyCode != KeyCode.None && lastKeyPressed != e.keyCode)
+            {
+                ProcessKey(e.keyCode);
+                
+                lastKeyPressed = e.keyCode;
+>>>>>>> parent of fe29ece (Fix)
             }
 
             if (e.isKey)
@@ -72,6 +106,7 @@ namespace Hangman.Scripts
                 
             }
         }
+<<<<<<< HEAD
 
         private void ProcessKey(KeyCode key) // передается клавиша параметром onGUI
         {
@@ -132,6 +167,68 @@ namespace Hangman.Scripts
             _textField.text = stringToPrint;
         }
     } 
+=======
+    }
+
+    private void ProcessKey(KeyCode key)
+    {
+        string word = "Time";
+        
+        print("Key Pressed:" + key);
+
+        char pressedKeyString = key.ToString()[0];
+
+        string wordUppercase = word.ToUpper();
+        
+        bool wordContainsPressedKey = word.Contains(pressedKeyString);
+        bool letterWasGuessed = guessedLetters.Contains(pressedKeyString);
+
+        if (!wordContainsPressedKey && wrongTriedLetter.Contains(pressedKeyString))
+        {
+            wrongTriedLetter.Add(pressedKeyString);
+            hp -= 1;
+            print("Wrong Letter \n Hp left =" + hp);
+
+            if (hp <= 0)
+            {
+                print("You Lost");
+            }
+            else
+            {
+                print("Wrong letter \n Hp left = " + hp);
+            }
+        }
+
+        if (wordContainsPressedKey && !letterWasGuessed)
+        {
+            guessedLetters.Add(pressedKeyString);
+        }
+        
+        string stringToPrint = "";
+        for (int i = 0; i < wordUppercase.Length; i++)
+        {
+            char letterInWord = wordUppercase[i];
+
+            if (guessedLetters.Contains(letterInWord))
+            {
+                stringToPrint += letterInWord;
+            }
+            else
+            {
+                stringToPrint += "_";
+            }
+        }
+
+        if (wordUppercase == stringToPrint)
+        {
+            print("You Win");
+        }
+        
+        // print(guessedLetters); - так не выводится
+        // print(string.Join(",", guessedLetters)); // выводит строчкой 4,5,6,7,8
+        print(stringToPrint);
+    }
+>>>>>>> parent of fe29ece (Fix)
 }
 
 
