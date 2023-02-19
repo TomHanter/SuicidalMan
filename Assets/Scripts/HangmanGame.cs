fe29ece4063df9797e57952f1ff2d4fc7508cebc
Vlad <tomhanter785@gmail.com>
2023-02-19 11:05:28 +0300

    Fix

--- Assets/Scripts/HangmanGame.cs
+++ Assets/Scripts/HangmanGame.cs
@@ -6,104 +6,107 @@ using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class HangmanGame : MonoBehaviour
+namespace Hangman.Scripts
 {
-    [SerializeField] TextMeshPro 
-    [SerializeField] private int hp = 7;
-    
-    private List<char> guessedLetters = new List<char>();
-    private List<char> wrongTriedLetter = new List<char>();
-
-    private string[] words =
+      public class HangmanGame : MonoBehaviour
     {
-        "Cat",
-        "Dog"
-    };
+        [SerializeField] private TextMeshProUGUI _textField;
+        [SerializeField] private int hp = 7;
 
-    private string wordToGuess = "";
+        private List<char> guessedLetters = new List<char>();
+        private List<char> wrongTriedLetter = new List<char>();
 
-    private KeyCode lastKeyPressed;
-
-    private void Start()
-    {
-        var randomIndex = Random.Range(0, words.Length);
-
-        wordToGuess = words[randomIndex];
-    }
-
-    void OnGUI()
-    {
-        Event e = Event.current;
-        if (e.isKey)
+        private string[] words =
         {
-            // Debug.Log("Detected key code: " + e.keyCode);
+            "Cat",
+            "Dog"
+        };
 
+        private string wordToGuess = "";
 
-            if (e.keyCode != KeyCode.None && lastKeyPressed != e.keyCode)
-            {
-                ProcessKey(e.keyCode);
-                
-                lastKeyPressed = e.keyCode;
-            }
+        private KeyCode lastKeyPressed;
+
+        private void Start()
+        {
+            var randomIndex = Random.Range(0, words.Length);
+
+            wordToGuess = words[randomIndex];
         }
-    }
 
-    private void ProcessKey(KeyCode key)
-    {
-        string word = "Time";
-        
-        print("Key Pressed:" + key);
-
-        char pressedKeyString = key.ToString()[0];
-
-        string wordUppercase = word.ToUpper();
-        
-        bool wordContainsPressedKey = word.Contains(pressedKeyString);
-        bool letterWasGuessed = guessedLetters.Contains(pressedKeyString);
-
-        if (!wordContainsPressedKey && wrongTriedLetter.Contains(pressedKeyString))
+        void OnGUI()
         {
-            wrongTriedLetter.Add(pressedKeyString);
-            hp -= 1;
-            print("Wrong Letter \n Hp left =" + hp);
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                // Debug.Log("Detected key code: " + e.keyCode);
 
-            if (hp <= 0)
-            {
-                print("You Lost");
-            }
-            else
-            {
-                print("Wrong letter \n Hp left = " + hp);
+                if (e.keyCode != KeyCode.None && lastKeyPressed != e.keyCode) // была нажата и не отпущена и не равно той, которая была на прошлой попытке
+                {
+                    ProcessKey(e.keyCode); // выводим обработку
+
+                    lastKeyPressed = e.keyCode; // буферим нажатую клавишу
+                }
             }
         }
 
-        if (wordContainsPressedKey && !letterWasGuessed)
+        private void ProcessKey(KeyCode key) // передается клавиша параметром onGUI
         {
-            guessedLetters.Add(pressedKeyString);
-        }
-        
-        string stringToPrint = "";
-        for (int i = 0; i < wordUppercase.Length; i++)
-        {
-            char letterInWord = wordUppercase[i];
 
-            if (guessedLetters.Contains(letterInWord))
-            {
-                stringToPrint += letterInWord;
-            }
-            else
-            {
-                stringToPrint += "_";
-            }
-        }
+            print("Key Pressed:" + key); // выводим клавишу которую нажали
 
-        if (wordUppercase == stringToPrint)
-        {
-            print("You Win");
+            char pressedKeyString = key.ToString()[0];
+
+            string wordUppercase = wordToGuess.ToUpper(); // переводит в заглав буквы
+
+            bool wordContainsPressedKey =
+                wordUppercase.Contains(
+                    pressedKeyString); // содерж ли загад слова кнопке, Cont проходит по каждой букве в строке и сравн которую передали
+            bool letterWasGuessed = guessedLetters.Contains(pressedKeyString); // провер была ли угадана буква ранее
+
+            if (!wordContainsPressedKey && !wrongTriedLetter.Contains(pressedKeyString))
+            {
+                wrongTriedLetter.Add(pressedKeyString);
+                hp -= 1;
+
+                if (hp <= 0)
+                {
+                    print("You Lost");
+                }
+                else
+                {
+                    print("Wrong letter \n Hp left = " + hp);
+                }
+            }
+
+            if (wordContainsPressedKey && !letterWasGuessed)
+            {
+                guessedLetters.Add(pressedKeyString);
+            }
+
+            string stringToPrint = ""; // формир строку которуб хотим вывести и что было угадана
+            for (int i = 0; i < wordUppercase.Length; i++) // проходим по всем буквам занад слова
+            {
+                char letterInWord = wordUppercase[i];
+
+                if (guessedLetters.Contains(letterInWord)) // если угад буква
+                {
+                    stringToPrint += letterInWord;
+                }
+                else // если не угад
+                {
+                    stringToPrint += "_";
+                }
+            }
+
+            if (wordUppercase == stringToPrint) // если угадали все слово
+            {
+                print("You Win");
+            }
+
+            // print(guessedLetters); - так не выводится
+            // print(string.Join(",", guessedLetters)); // выводит строчкой 4,5,6,7,8
+            print(stringToPrint);
+            _textField.text = stringToPrint;
         }
-        
-        // print(guessedLetters); - так не выводится
-        // print(string.Join(",", guessedLetters)); // выводит строчкой 4,5,6,7,8
-        print(stringToPrint);
     }
 }
